75b6628b92a01e80b6e90e0596aea789a12eef00
hkawabata <meiwaku_mail_iyaaaaa@yahoo.co.jp>
2016-09-17 21:30:09 +0900

    refactor

--- script/data_set_generator/data_set_generator.py
+++ script/data_set_generator/data_set_generator.py
@@ -3,6 +3,7 @@
 import numpy.random
 import random
 
+
 def generate_two_circle_data_set(num, x1=1, y1=1, r1=1, x2=-1, y2=-1, r2=1):
     data_set = []
     for i in range(num / 2):
@@ -20,4 +21,18 @@ def generate_two_circle_data_set(num, x1=1, y1=1, r1=1, x2=-1, y2=-1, r2=1):
             y = numpy.random.rand() * r2 * 2 - r2
         data_set.append(([x + x2, y + y2], -1))
     random.shuffle(data_set)
+    return data_set
+
+
+def generate_square_data_set(num, x1, y1, x2, y2, a):
+    x_width = x2 - x1
+    y_width = y2 - y1
+    x = [x1 + _x * x_width for _x in numpy.random.rand(num)]
+    y = [y1 + _y * y_width for _y in numpy.random.rand(num)]
+    x_center = (x1 + x2) / 2.0
+    y_center = (y1 + y2) / 2.0
+    data_set = []
+    for _x, _y in zip(x, y):
+        label = 1 if a * (_x - x_center) + y_center < _y else -1
+        data_set.append(([_x, _y], label))
     return data_set
--- script/perceptron/perceptron.py
+++ script/perceptron/perceptron.py
@@ -8,7 +8,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../data_set_gener
 import data_set_generator
 
 
-eta = 0.1
+eta = 0.01
 
 errors = []
 
@@ -31,34 +31,38 @@ def execute(training_data, training_labels):
     training_data_with_dummy = insert_dummy_elem(training_data)
     w = generate_random_weight(len(training_data_with_dummy[0]))
 
-    for _ in range(10):
+    for time in range(100):
         cnt_err = 0
-        cnt_correct = 0
         for xi, labeli in zip(training_data_with_dummy, training_labels):
-            z = 0
-            for xij, wj in zip(xi, w):
-                z += xij * wj
-            update = eta * (labeli - activation_func(z))
+            update = eta * (labeli - calculate_label(xi, w))
 
             for j in range(len(w)):
                 w[j] += update * xi[j]
             if update != 0:
                 cnt_err += 1
-            else:
-                cnt_correct += 1
-        print cnt_err, cnt_correct, w
+        if cnt_err == 0:
+            print "{0} errors".format(cnt_err)
+            print "converged after {0} trial".format(time)
+            break
+        else:
+            print "{0} errors".format(cnt_err)
     return w
 
 
-def calculate_labels(test_data, w):
-    test_data_with_dummy = insert_dummy_elem(test_data)
+def calculate_labels(data, w):
+    data_with_dummy = insert_dummy_elem(data)
     labels = []
-    for j in range(len(test_data_with_dummy)):
-        z = 0
-        for i in range(len(test_data_with_dummy[0])):
-            z += test_data_with_dummy[j][i] * w[i]
-        labels.append(activation_func(z))
+    for j in range(len(data_with_dummy)):
+        label = calculate_label(data_with_dummy[j], w)
+        labels.append(label)
     return labels
+
+
+def calculate_label(data_i, w):
+    z = 0
+    for j in range(len(data_i)):
+        z += data_i[j] * w[j]
+    return activation_func(z)
 
 
 def print_result(data, labels):
@@ -74,21 +78,16 @@ def print_result(data, labels):
     plt.show()
 
 
-
 def test():
     training_data = [[1,2],[2,3],[1,4],[-1,0],[-5,-3],[-2,-1],[4,5],[-6,-5],
                     [1,0],[2,1],[1,-3],[-1,-3],[-5,-8],[-2,-4],[4,3],[-6,-7]]
-
     training_labels = [1,1,1,1,1,1,1,1,
               -1,-1,-1,-1,-1,-1,-1,-1]
-
     test_data = [[1,4],[1,-2],[3,4],[4,2],[-1,8],[-1,-4],[-5,-2],[-5,-9],[-2,0],[-2,-3]]
     test_labels = [1,-1,1,-1,1,-1,1,-1,1,-1]
 
     w = execute(training_data, training_labels)
-
     labels = calculate_labels(test_data, w)
-
     print_result(test_data, labels)
 
 
@@ -96,8 +95,18 @@ def test2():
     data_set = data_set_generator.generate_two_circle_data_set(500, x1=1, y1=2, r1=1, x2=-1, y2=3, r2=1)
     data = [x[0] for x in data_set]
     labels = [x[1] for x in data_set]
+    do_test(data, labels)
 
-    num_of_traning_data = len(data_set)*1/3
+
+def test3():
+    data_set = data_set_generator.generate_square_data_set(1000, 0, 0, 5, 5, 2)
+    data = [x[0] for x in data_set]
+    labels = [x[1] for x in data_set]
+    do_test(data, labels)
+
+
+def do_test(data, labels):
+    num_of_traning_data = len(data)*1/3
     training_data = data[:num_of_traning_data]
     training_labels = labels[:num_of_traning_data]
     test_data = data[num_of_traning_data:]
@@ -108,10 +117,8 @@ def test2():
 
     print_result(test_data, result_labels)
 
+
 if __name__ == '__main__':
     test()
     test2()
-
-
-
-
+    test3()
