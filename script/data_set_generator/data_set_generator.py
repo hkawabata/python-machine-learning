f72b30977a3f213129e1abd2e255850777061b13
hkawabata <meiwaku_mail_iyaaaaa@yahoo.co.jp>
2016-09-24 22:40:57 +0900

    fix bug

--- script/data_set_generator/data_set_generator.py
+++ script/data_set_generator/data_set_generator.py
@@ -9,14 +9,14 @@ def generate_two_circle_data_set(num, x1=1, y1=1, r1=1, x2=-1, y2=-1, r2=1):
     for i in range(num / 2):
         x = numpy.random.rand() * r1 * 2 - r1
         y = numpy.random.rand() * r1 * 2 - r1
-        while x * x + y * y > r1:
+        while x * x + y * y > r1 * r1:
             x = numpy.random.rand() * r1 * 2 - r1
             y = numpy.random.rand() * r1 * 2 - r1
         data_set.append(([x + x1, y + y1], 1))
     for i in range(num - num / 2):
         x = numpy.random.rand() * r2 * 2 - r2
         y = numpy.random.rand() * r2 * 2 - r2
-        while x * x + y * y > r2:
+        while x * x + y * y > r2 * r2:
             x = numpy.random.rand() * r2 * 2 - r2
             y = numpy.random.rand() * r2 * 2 - r2
         data_set.append(([x + x2, y + y2], -1))
