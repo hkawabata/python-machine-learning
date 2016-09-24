bb444eaad3061d539373221bb672ae1b730f2c88
hkawabata <meiwaku_mail_iyaaaaa@yahoo.co.jp>
2016-09-24 22:36:04 +0900

    refactoring

--- script/perceptron/perceptron.py
+++ script/perceptron/perceptron.py
@@ -9,57 +9,76 @@ import data_set_generator
 
 
 class Perceptron:
-    errors = []
+    """パーセプトロンの分類器
+
+    パラメータ
+    ----------
+    eta : float
+        学習率 (0, 1.0]
+    n_iter : int
+        トレーニングの最大反復回数
+
+    属性
+    ----------
+    w_ : 適合後の重み
+    errors_ : 各エポックでの誤分類の数
+    """
 
     def __init__(self, eta, n_iter):
         self.eta = eta
         self.n_iter = n_iter
+        self.errors_ = []
+        self.w_ = []
 
-    def generate_random_weight(self, num_elem):
+    @staticmethod
+    def generate_random_weight(num_elem):
         return map(lambda r: r - 0.5, random.rand(num_elem))
 
-    def insert_dummy_elem(self, x):
+    @staticmethod
+    def insert_dummy_elem(x):
         return map(lambda v: [1] + v, x)
 
-    def activation_func(self, z):
+    @staticmethod
+    def activation_func(z):
         if z > 0:
             return 1
         else:
             return -1
 
-    def execute(self, training_data, training_labels):
+    def fit(self, training_data, training_labels):
         training_data_with_dummy = self.insert_dummy_elem(training_data)
-        w = self.generate_random_weight(len(training_data_with_dummy[0]))
+        self.errors_ = []
+        self.w_ = self.generate_random_weight(len(training_data_with_dummy[0]))
 
-        for time in range(100):
+        for time in range(self.n_iter):
             cnt_err = 0
             for xi, labeli in zip(training_data_with_dummy, training_labels):
-                update = self.eta * (labeli - self.calculate_label(xi, w))
+                update = self.eta * (labeli - self.calculate_label(xi))
 
-                for j in range(len(w)):
-                    w[j] += update * xi[j]
+                for j in range(len(self.w_)):
+                    self.w_[j] += update * xi[j]
                 if update != 0:
                     cnt_err += 1
+            self.errors_.append(cnt_err)
             if cnt_err == 0:
                 print "{0} errors".format(cnt_err)
                 print "converged after {0} trial".format(time)
                 break
             else:
                 print "{0} errors".format(cnt_err)
-        return w
 
-    def calculate_labels(self, data, w):
+    def calculate_labels(self, data):
         data_with_dummy = self.insert_dummy_elem(data)
         labels = []
         for j in range(len(data_with_dummy)):
-            label = self.calculate_label(data_with_dummy[j], w)
+            label = self.calculate_label(data_with_dummy[j])
             labels.append(label)
         return labels
 
-    def calculate_label(self, data_i, w):
+    def calculate_label(self, data_i):
         z = 0
         for j in range(len(data_i)):
-            z += data_i[j] * w[j]
+            z += data_i[j] * self.w_[j]
         return self.activation_func(z)
 
     def print_result(self, data, labels):
@@ -67,29 +86,39 @@ class Perceptron:
         distinct_labels = list(set(labels))
         data_and_labels = zip(data, labels)
         colors = ["red", "blue", "green", "orange", "navy", "purple", "yellow", "violet", "tomato", "cyan", "pink"]
+        plt.subplot(1, 2, 1)
+        plt.title("result of classification")
         for label, color in zip(distinct_labels, colors):
             x = [d[0][0] for d in data_and_labels if d[1] == label]
             y = [d[0][1] for d in data_and_labels if d[1] == label]
             plt.scatter(x, y, color=color, label=label)
+        plt.xlabel('x')
+        plt.ylabel('y')
         plt.legend(loc='upper left')
+
+        plt.subplot(1, 2, 2)
+        plt.plot(list(range(1, len(self.errors_) + 1)), self.errors_, marker='o')
+        plt.xlabel('epochs')
+        plt.ylabel("number of mis-classifications")
+        plt.xlim(0)
+        plt.ylim(0)
         plt.show()
 
 
 class PerceptronTest:
-    def __init__(self, perceptron):
+
+    def __init__(self, perceptron, training_data_rate=0.5):
         self.ppn = perceptron
+        self.td_rate = training_data_rate
 
     def test(self):
-        training_data = [[1,2],[2,3],[1,4],[-1,0],[-5,-3],[-2,-1],[4,5],[-6,-5],
-                        [1,0],[2,1],[1,-3],[-1,-3],[-5,-8],[-2,-4],[4,3],[-6,-7]]
-        training_labels = [1,1,1,1,1,1,1,1,
-                  -1,-1,-1,-1,-1,-1,-1,-1]
-        test_data = [[1,4],[1,-2],[3,4],[4,2],[-1,8],[-1,-4],[-5,-2],[-5,-9],[-2,0],[-2,-3]]
-        test_labels = [1,-1,1,-1,1,-1,1,-1,1,-1]
-
-        w = self.ppn.execute(training_data, training_labels)
-        labels = self.ppn.calculate_labels(test_data, w)
-        self.ppn.print_result(test_data, labels)
+        data = [[1,2],[2,3],[-5,-8],[-2,-4],[1,4],[-1,0],[1,-3],[-1,-3],
+                [1,0],[2,1],[4,5],[-6,-5],[4,3],[-6,-7],[-5,-3],[-2,-1],
+                [1,4],[1,-2],[3,4],[4,2],[-1,8],[-1,-4],[-5,-2],[-5,-9],[-2,0],[-2,-3]]
+        labels = [1,1,-1,-1,1,1,-1,-1,
+                  -1,-1,1,1,-1,-1,1,1,
+                  1,-1,1,-1,1,-1,1,-1,1,-1]
+        self.do_test(data, labels)
 
     def test2(self):
         data_set = data_set_generator.generate_two_circle_data_set(500, x1=1, y1=2, r1=1, x2=-1, y2=3, r2=1)
@@ -98,27 +127,27 @@ class PerceptronTest:
         self.do_test(data, labels)
 
     def test3(self):
-        data_set = data_set_generator.generate_square_data_set(1000, 0, 0, 5, 5, 2)
+        data_set = data_set_generator.generate_square_data_set(1000, x1=0, y1=0, x2=5, y2=5, a=2)
         data = [x[0] for x in data_set]
         labels = [x[1] for x in data_set]
         self.do_test(data, labels)
 
     def do_test(self, data, labels):
-        num_of_traning_data = len(data)*1/3
+        num_of_traning_data = int(len(data) * self.td_rate)
         training_data = data[:num_of_traning_data]
         training_labels = labels[:num_of_traning_data]
         test_data = data[num_of_traning_data:]
         test_labels = labels[num_of_traning_data:]
 
-        w = self.ppn.execute(training_data, training_labels)
-        result_labels = self.ppn.calculate_labels(test_data, w)
+        w = self.ppn.fit(training_data, training_labels)
+        result_labels = self.ppn.calculate_labels(test_data)
 
         self.ppn.print_result(test_data, result_labels)
 
 
 if __name__ == '__main__':
-    ppn = Perceptron(eta=0.01, n_iter=10)
-    ppn_test = PerceptronTest(ppn)
+    ppn = Perceptron(eta=0.01, n_iter=100)
+    ppn_test = PerceptronTest(ppn, training_data_rate=1.0/3)
     ppn_test.test()
     ppn_test.test2()
     ppn_test.test3()
