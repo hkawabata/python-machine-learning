9cff710e4bd108e419c14003f932497aca31ce6a
hkawabata <meiwaku_mail_iyaaaaa@yahoo.co.jp>
2016-09-25 19:38:58 +0900

    display contour

--- script/classification/classifier.py
+++ script/classification/classifier.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 
 import sys
 import os
@@ -37,7 +38,7 @@ class Classifier:
         for data in data_with_dummy:
             label = self.predict_label(data)
             labels.append(label)
-        return labels
+        return np.array(labels)
 
     def predict_label(self, data_i):
         z = 0
@@ -45,28 +46,41 @@ class Classifier:
             z += data_i[j] * self.w_[j]
         return self.activation_func(z)
 
-    def print_result(self, data, labels):
+    def print_result(self, data, resolution=0.02):
         u"""2次元ベクトルを想定"""
-        distinct_labels = list(set(labels))
-        data_and_labels = zip(data, labels)
+        # データのラベルを計算
+        data_labels = self.predict_labels(data)
+        # 等高線用のグリッドデータを作成し、各ブロックのラベルを計算
+        x_min, x_max = data[:, 0].min() - 1, data[:, 0].max() + 1
+        y_min, y_max = data[:, 1].min() - 1, data[:, 1].max() + 1
+        x_grid, y_grid = np.meshgrid(np.arange(x_min, x_max, resolution), np.arange(y_min, y_max, resolution))
+        grid = np.array(zip(x_grid.ravel(), y_grid.ravel()))
+        grid_labels = self.predict_labels(grid).reshape(x_grid.shape)
+
+        unique_labels = np.unique(data_labels)
+        data_and_labels = zip(data, data_labels)
         colors = ["red", "blue", "green", "orange", "navy", "purple", "yellow", "violet", "tomato", "cyan", "pink"]
+        cmap = ListedColormap(colors[:len(unique_labels)])
 
         # 分類結果
         plt.subplot(1, 2, 1)
-        plt.title("result of classification")
-        for label, color in zip(distinct_labels, colors):
-            x = [d[0][0] for d in data_and_labels if d[1] == label]
-            y = [d[0][1] for d in data_and_labels if d[1] == label]
-            plt.scatter(x, y, color=color, label=label)
+        plt.title('result of classification')
         plt.xlabel('x')
         plt.ylabel('y')
         plt.legend(loc='upper left')
+        plt.contourf(x_grid, y_grid, grid_labels, alpha=0.4, cmap=cmap)
+        for label, color in zip(unique_labels, colors):
+            x = [d[0][0] for d in data_and_labels if d[1] == label]
+            y = [d[0][1] for d in data_and_labels if d[1] == label]
+            plt.scatter(x, y, color=color, label=label)
+        plt.xlim(x_min, x_max)
+        plt.ylim(y_min, y_max)
 
         # 誤分類の数の遷移
         plt.subplot(1, 2, 2)
-        plt.plot(list(range(1, len(self.errors_) + 1)), self.errors_, marker='o')
         plt.xlabel('epochs')
-        plt.ylabel("number of mis-classifications")
+        plt.ylabel('number of mis-classifications')
+        plt.plot(list(range(1, len(self.errors_) + 1)), self.errors_, marker='o')
         plt.xlim(0)
         plt.ylim(0)
         plt.show()
@@ -93,19 +107,17 @@ class ClassifierTest:
         self.do_test(data, labels)
 
     def test3(self):
-        data_set = data_set_generator.generate_square_data_set(1000, x1=0, y1=0, x2=5, y2=5, a=2)
+        data_set = data_set_generator.generate_square_data_set(500, x1=0, y1=0, x2=5, y2=5, a=2)
         data = [x[0] for x in data_set]
         labels = [x[1] for x in data_set]
         self.do_test(data, labels)
 
     def do_test(self, data, labels):
         num_of_traning_data = int(len(data) * self.td_rate)
-        training_data = data[:num_of_traning_data]
-        training_labels = labels[:num_of_traning_data]
-        test_data = data[num_of_traning_data:]
-        test_labels = labels[num_of_traning_data:]
+        training_data = np.array(data[:num_of_traning_data])
+        training_labels = np.array(labels[:num_of_traning_data])
+        test_data = np.array(data[num_of_traning_data:])
+        test_labels = np.array(labels[num_of_traning_data:])
 
         self.classifier.fit(training_data, training_labels)
-        result_labels = self.classifier.predict_labels(test_data)
-
-        self.classifier.print_result(test_data, result_labels)
+        self.classifier.print_result(test_data)
