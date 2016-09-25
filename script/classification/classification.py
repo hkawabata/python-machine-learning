8a0b685940c93559f3733e57d52263a7c6041e89
hkawabata <meiwaku_mail_iyaaaaa@yahoo.co.jp>
2016-09-25 12:05:57 +0900

    refactoring

--- script/classification/classification.py
+++ script/classification/classification.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+from numpy import random
+import matplotlib.pyplot as plt
+
 
 class Classification:
     def __init__(self, eta, n_iter):
@@ -7,3 +10,55 @@ class Classification:
         self.n_iter = n_iter
         self.errors_ = []
         self.w_ = []
+
+    @staticmethod
+    def generate_random_weight(num_elem):
+        return map(lambda r: r - 0.5, random.rand(num_elem))
+
+    @staticmethod
+    def insert_dummy_elem(x):
+        return map(lambda v: [1] + v, x)
+
+    @staticmethod
+    def activation_func(z):
+        if z > 0:
+            return 1
+        else:
+            return -1
+
+    def calculate_labels(self, data):
+        data_with_dummy = self.insert_dummy_elem(data)
+        labels = []
+        for j in range(len(data_with_dummy)):
+            label = self.calculate_label(data_with_dummy[j])
+            labels.append(label)
+        return labels
+
+    def calculate_label(self, data_i):
+        z = 0
+        for j in range(len(data_i)):
+            z += data_i[j] * self.w_[j]
+        return self.activation_func(z)
+
+    def print_result(self, data, labels):
+        u"""2次元ベクトルを想定"""
+        distinct_labels = list(set(labels))
+        data_and_labels = zip(data, labels)
+        colors = ["red", "blue", "green", "orange", "navy", "purple", "yellow", "violet", "tomato", "cyan", "pink"]
+        plt.subplot(1, 2, 1)
+        plt.title("result of classification")
+        for label, color in zip(distinct_labels, colors):
+            x = [d[0][0] for d in data_and_labels if d[1] == label]
+            y = [d[0][1] for d in data_and_labels if d[1] == label]
+            plt.scatter(x, y, color=color, label=label)
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.legend(loc='upper left')
+
+        plt.subplot(1, 2, 2)
+        plt.plot(list(range(1, len(self.errors_) + 1)), self.errors_, marker='o')
+        plt.xlabel('epochs')
+        plt.ylabel("number of mis-classifications")
+        plt.xlim(0)
+        plt.ylim(0)
+        plt.show()
--- script/classification/perceptron/perceptron.py
+++ script/classification/perceptron/perceptron.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from numpy import random
-import matplotlib.pyplot as plt
-import sys
 import os
+import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../data_set_generator')
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../classification')
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
 import data_set_generator
 from classification import Classification
 
@@ -29,21 +27,6 @@ class Perceptron(Classification):
     def __init__(self, eta, n_iter):
         Classification.__init__(self, eta, n_iter)
 
-    @staticmethod
-    def generate_random_weight(num_elem):
-        return map(lambda r: r - 0.5, random.rand(num_elem))
-
-    @staticmethod
-    def insert_dummy_elem(x):
-        return map(lambda v: [1] + v, x)
-
-    @staticmethod
-    def activation_func(z):
-        if z > 0:
-            return 1
-        else:
-            return -1
-
     def fit(self, training_data, training_labels):
         training_data_with_dummy = self.insert_dummy_elem(training_data)
         self.errors_ = []
@@ -65,43 +48,6 @@ class Perceptron(Classification):
                 break
             else:
                 print "{0} errors".format(cnt_err)
-
-    def calculate_labels(self, data):
-        data_with_dummy = self.insert_dummy_elem(data)
-        labels = []
-        for j in range(len(data_with_dummy)):
-            label = self.calculate_label(data_with_dummy[j])
-            labels.append(label)
-        return labels
-
-    def calculate_label(self, data_i):
-        z = 0
-        for j in range(len(data_i)):
-            z += data_i[j] * self.w_[j]
-        return self.activation_func(z)
-
-    def print_result(self, data, labels):
-        u"""2次元ベクトルを想定"""
-        distinct_labels = list(set(labels))
-        data_and_labels = zip(data, labels)
-        colors = ["red", "blue", "green", "orange", "navy", "purple", "yellow", "violet", "tomato", "cyan", "pink"]
-        plt.subplot(1, 2, 1)
-        plt.title("result of classification")
-        for label, color in zip(distinct_labels, colors):
-            x = [d[0][0] for d in data_and_labels if d[1] == label]
-            y = [d[0][1] for d in data_and_labels if d[1] == label]
-            plt.scatter(x, y, color=color, label=label)
-        plt.xlabel('x')
-        plt.ylabel('y')
-        plt.legend(loc='upper left')
-
-        plt.subplot(1, 2, 2)
-        plt.plot(list(range(1, len(self.errors_) + 1)), self.errors_, marker='o')
-        plt.xlabel('epochs')
-        plt.ylabel("number of mis-classifications")
-        plt.xlim(0)
-        plt.ylim(0)
-        plt.show()
 
 
 class PerceptronTest:
